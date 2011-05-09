0fb30965bced25eb3437416c79455e09fcadbfcf
Hasan Kumar Reddy A <mintuhouse@gmail.com>
2011-05-09 11:23:11 +0530

    Initial Comit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Ruby on Rails Tutorial : demo application
+
+This is a demo for [*Learn from Rails:Example*](http://rails.org) by [Hasan Kumar Reddy](http://www.cse.iitb.ac.in/~hasan).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '2b20a0da70b9a4fbdda82b84a7c3a8b73b4788cd0cc9aaa6deaf241965427e4c55fa3df1a6e09e44261b697ab6e6cd9fdf9607f63d582d3dddd55fbd641e1675'
