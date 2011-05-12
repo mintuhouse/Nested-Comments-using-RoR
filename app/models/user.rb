10945aa31d68308b2e6fda2ac7b1241ff986e86d
Hasan Kumar Reddy A <mintuhouse@gmail.com>
2011-05-12 14:46:39 +0530

    Added user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,8 @@ require 'digest'
 class User < ActiveRecord::Base
   attr_accessor   :password
   attr_accessible :name, :email, :password, :password_confirmation
+  
+  has_many :microposts, :dependent => :destroy
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -33,6 +35,11 @@ class User < ActiveRecord::Base
   # Return true if the user's password matches the submitted password.
   def has_password?(submitted_password)
     encrypted_password == encrypt(submitted_password)
+  end
+  
+  def feed
+    # This is preliminary. See Chapter 12 for the full implementation.
+    Micropost.where("user_id = ?", id)
   end
   
   class << self
